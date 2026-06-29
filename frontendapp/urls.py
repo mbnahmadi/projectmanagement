3308cfe718bbd498b9c49f7ca0ebc796205953fa
mbnahmadi <mbn.ahmadi78@gmail.com>
2026-06-29 17:27:03 +0330

    remove dashboad url

--- frontendapp/urls.py
+++ frontendapp/urls.py
@@ -20,5 +20,5 @@ from . import views
 
 urlpatterns = [
     path('home/', views.home_view, name='home'),
-    path('dashboard/', views.dashboard_view, name='home'),
+    # path('dashboard/', views.dashboard_view, name='home'),
 ]
